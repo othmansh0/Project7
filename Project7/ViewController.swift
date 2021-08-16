7580eecc9d5dd426c44d8956b5a3b4e6d6d9ac94
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-16 13:32:17 +0300

    Parse JSON using Codable

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -13,8 +13,29 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString){
+            if let data = try? Data(contentsOf: url){
+                //we're OK to parse
+                parse(json: data)
+            }
+        }
         
   
+    }
+    
+    
+    func parse(json: Data){
+        //dedicated to converting between JSON and Codable objects.
+        let decoder = JSONDecoder()
+        // asking it to convert our json data into a Petitions object,Petitions.self to refer to the type itself rather than an instance
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -23,10 +44,12 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        cell.textLabel?.text = petitions[indexPath.row].title
+        cell.detailTextLabel?.text = petitions[indexPath.row].body
         return cell
     }
+    
+    
 
 }
 
