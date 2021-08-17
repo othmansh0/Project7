59a7ab058f0c0956d26a59dcb4992e70342e2dcf
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-17 14:39:17 +0300

    Add filter petitions feature

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -9,16 +9,17 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
-    
+    var originalPetitions = [Petition]()
+    var numberOfFilters = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
         
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterPetitions))
         
         let urlString: String
-
+        
         if navigationController?.tabBarItem.tag == 0 {
             // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -35,8 +36,41 @@ class ViewController: UITableViewController {
             }
         }
         showError()
-  
+        
     }
+    
+    @objc func filterPetitions(){
+        numberOfFilters += 1
+        let ac = UIAlertController(title: "Filter", message: "Enter a Keyword", preferredStyle: .alert)
+        ac.addTextField()
+        var filteredPetitions = [Petition]()
+        let action = UIAlertAction(title: "Search", style: .default)   {        [weak self] action in
+            //If user tries to filter data again
+            if(self!.numberOfFilters > 1){
+                filteredPetitions.removeAll()
+                //instead of parsing data in petitions again
+                //created another ptetions that never changes
+                self?.petitions  = self?.originalPetitions ?? []
+            }
+            guard let keyWord = ac.textFields?[0].text else { return }
+            print(keyWord)
+            for petition in self?.petitions ?? [] {
+                if(petition.body.contains(keyWord) || petition.title.contains(keyWord) ){
+                    filteredPetitions.append(petition)
+                }
+            }
+            self?.petitions = filteredPetitions
+            self?.tableView.reloadData()
+        }
+        ac.addAction(action)
+        present(ac, animated: true)
+        
+        
+        
+        
+        
+    }
+    
     @objc func showCredits(){
         let ac = UIAlertController(title: "Credits", message: "data comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
@@ -57,11 +91,12 @@ class ViewController: UITableViewController {
         // asking it to convert our json data into a Petitions object,Petitions.self to refer to the type itself rather than an instance
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
             petitions = jsonPetitions.results
+            originalPetitions = petitions
             tableView.reloadData()
         }
         
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
@@ -81,6 +116,6 @@ class ViewController: UITableViewController {
     }
     
     
-
+    
 }
 
